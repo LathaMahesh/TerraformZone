13bd18045ce38093e55d312676d40a98f86e084e
Latha <lathakm.1993@gmail.com>
2021-01-26 12:04:50 +0530

    added vpc and subnets

--- provider.tf
+++ provider.tf
@@ -2,18 +2,17 @@ provider "aws" {
     region      = "us-west-2"
 }
 
-resource "aws_vpc" "lathavpc" {
+resource "aws_vpc" "lvpc" {
     cidr_block = "192.168.0.0/16"
 
     tags = {
-        Name        = "lathavpc"
+      Name = "lathaVpc"
     }
-  
 }
 
 resource "aws_subnet" "Web" {
-    vpc_id = "aws_vpc.lathavpc.id"
-    cidr_block = "192.168.1.0/24"
+    vpc_id          = aws_vpc.lvpc.id
+    cidr_block      = "192.168.1.0/24"
     
 
     tags = {
@@ -22,10 +21,10 @@ resource "aws_subnet" "Web" {
 }
 
 resource "aws_subnet" "App" {
-    vpc_id = "aws_vpc.lathavpc.id"
-    cidr_block = "192.168.2.0/24"
+    vpc_id          = aws_vpc.lvpc.id
+    cidr_block      = "192.168.2.0/24"
 
     tags = {
       Name  = "App"
     }
-    }
+}
